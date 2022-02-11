4aaaaee54d28a8f1bb4c45e25731fcc6cf48765a
sayan das <sayanwastaken@gmail.com>
2022-02-12 00:57:37 +0530

    final portfolio

---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/Sayan_Das.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const resumeLink =
+  "https://github.com/sayanwastaken/resume/blob/main/fw13_356_Sayan_Das.pdf";
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button variant="primary" href="https://drive.google.com/drive/u/0/folders/1HZdTrrR5fKhDeSRmTaU3aaDw6--y5NR0" target="_blank">
+            <AiOutlineDownload />
+            &nbsp;Download Resume
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
